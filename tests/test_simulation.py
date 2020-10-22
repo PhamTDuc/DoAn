0ec68ec56897b06949ee6ace289d5cbc06ff32e0
PhamDuc <phamduc0701@gmail.com>
2020-10-22 15:01:24 +0700

    + Add test_calculateObservers

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -5,13 +5,21 @@ from package.orbital import Simulation
 import numpy as np
 import numpy.testing as npt
 
-
-def test_Simulation():
-    OE = TypeOE(semimajor_axis=6876.6644, eccentricity=0.0020122, inclination=1.6971285, right_ascension=2.7791209, argument_of_perigee=1.6541096, mean_anomaly=0.2653564)
+@pytest.fixture
+def getSimulation():
+    OE = TypeOE(semimajor_axis=6876.6644, eccentricity=0.0020122, inclination=1.6971285, \
+    right_ascension=2.7791209, argument_of_perigee=1.6541096, mean_anomaly=0.2653564)
     observer = TypeLatLong(0, 0)
     time = datetime(year=2020, month=4, day=15, hour=18, minute=22, second=4)
 
-    sim = Simulation(OE, observer, time=time)
-    observer_0 = sim.getAllCoords(3900)[0]
-    npt.assert_almost_equal(observer_0, np.array([-2936.2922, -5654.01, 0], dtype=float), decimal=2)
+    return Simulation(OE, observer, time=time)
 
+@pytest.mark.xfail
+@pytest.mark.parametrize("dt, true_result",[(3900, np.array([-2936.2922, -5654.01, 0])),\
+(4000,np.array([-389.5577, -6359.079, 0])),\
+(4100,np.array([2224.1957, -5970.1419, 0]))])
+def test_calulateObservers(getSimulation, dt:float, true_result):
+
+    sim = getSimulation
+    observer = sim.getAllCoords(dt)[0]
+    npt.assert_almost_equal(observer, true_result, decimal=2)
