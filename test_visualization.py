f1f0e4832ce5e065507e4f10b1ee35f7ead4d977
PhamDuc <phamduc0701@gmail.com>
2020-11-30 18:33:37 +0700

    - Modify visualization.py for testing

--- test_visualization.py
+++ test_visualization.py
@@ -8,11 +8,13 @@ y = np.arccos(x)
 figure, axes = plt.subplots(1, 2, constrained_layout=False)
 
 axes[0].plot(x, y)
-axes[0].set_title("Title for plot 01", fontsize=40)
+axes[0].set_title("Title for plot 01", fontsize=16)
 axes[0].set_xlabel("X", color="red")
 axes[0].set_xlim([-2, 2])
 ticks = np.linspace(-2, 2, num=10)
 axes[0].set_xticks(ticks)
 axes[0].set_xticklabels([f"{value:.2f}" for value in ticks])
+
+axes[0].axhline(y=1, xmin=-2, xmax=2, color = 'red', linewidth = 0.5, linestyle='--')
 
 plt.show()
